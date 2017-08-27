dfc59e3685feac7b61ffcabde01d6563a97f931b
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-08-27 14:19:29 -0500

    Populating page, created two directives

--- app/app.js
+++ app/app.js
@@ -6,9 +6,62 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+])
+    .controller('ReadingListController', function () {
+       this.books = books;
+       this.genres = genres;
+    })
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    .directive('bookGenres', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'partials/book-genres.html'
+        }
+    })
+
+    .directive('bookCover', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'partials/book-cover.html',
+            replace: true
+        }
+    });
+// config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+//   $locationProvider.hashPrefix('!');
+//
+//   $routeProvider.otherwise({redirectTo: '/view1'});
+// }]);
+
+var genres = [ 'fable', 'fantasy', 'fiction', 'folklore', 'horror', 'humor', 'legend', 'metafiction', 'mystery', 'mythology', 'non-fiction', 'poetry' ];
+
+var books = [
+    {
+        title: 'A Game of Thrones: A Song of Ice and Fire',
+        author: 'George R.R. Martin',
+        isbn: '0553593714',
+        review: 'The most inventive and entertaining fantasy saga of our time—warrants one hell of an introduction. I loved this book!',
+        rating: 4,
+        genres: { 'non-fiction': true, fantasy: true }
+    },{
+        title: 'HTML for Babies',
+        author: 'John C Vanden-Heuvel Sr',
+        isbn: '0615487661',
+        review: "It's never too early to be standards compliant! I taught my little one mark-up in under one hour!",
+        rating: 5,
+        genres: { fiction: true }
+    },{
+        title: 'A is for Array',
+        author: 'Brandon J Hansen',
+        isbn: '1489522212',
+        review: 'A is for Array is the ABC book for future programmers. Filled with fun illustrations and simple real-world examples, my children loved seeing my world intertwined with theirs!',
+        rating: 4,
+        genres: { fiction: true }
+    },{
+        title: 'The Dragon Reborn',
+        author: 'Robert Jordan',
+        isbn: '0812513711',
+        review: 'The Wheel weaves as the Wheel wills, and we are only the thread of the Pattern. Moiraine',
+        rating: 4,
+        genres: { 'non-fiction': true, fantasy: true }
+    }
+];
